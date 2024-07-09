7738512d26505f8293e76ce89778ce0643a0594e
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-07-09 20:22:43 -0300

    Exemplo 1: Adição, Contains e Exibição da lista com HashSet.

---
+++ Conjuntos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Conjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (string s in set)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
