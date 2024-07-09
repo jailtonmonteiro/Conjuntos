82bb550038801a785a883bf2e516723329224db3
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-07-09 20:40:51 -0300

    Exemplo 2: Uso da função SortedSet e aplicação algébrica.

--- Conjuntos/Program.cs
+++ Conjuntos/Program.cs
@@ -7,19 +7,35 @@ namespace Conjuntos
     {
         static void Main(string[] args)
         {
-            HashSet<string> set = new HashSet<string>();
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 0, 1, 3, 5, 7, 9, 10 };
 
-            set.Add("TV");
-            set.Add("Notebook");
-            set.Add("Tablet");
+            //Union
+            SortedSet<int> u = new SortedSet<int>(a);
+            u.UnionWith(b);
+            Console.Write("Union: ");
+            PrintCollection(u);
 
-            Console.WriteLine(set.Contains("Notebook"));
+            //Intersection
+            SortedSet<int> i = new SortedSet<int>(a);
+            i.IntersectWith(b);
+            Console.Write("Intersection: ");
+            PrintCollection(i);
 
-            foreach (string s in set)
+            //Difference
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.ExceptWith(b);
+            Console.Write("Difference: ");
+            PrintCollection(d);
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
             {
-                Console.WriteLine(s);
+                Console.Write(obj + " ");
             }
-
+            Console.WriteLine("");
         }
     }
 }
